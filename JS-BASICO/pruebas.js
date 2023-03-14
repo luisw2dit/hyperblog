a2c4c749b1a8d6596c3cbf073e2ac65161b83d73
Luis Alfredo Muñoz Zabaleta <luis.munoz@w2dit.com>
2023-03-14 18:06:30 -0500

    MATERIAL DE ESTUDIO JAVA SCRIPT VIDEO 16

---
+++ JS-BASICO/pruebas.js
@@ -0,0 +1,14 @@
+console.log (miNombre);
+
+var miNombre = "Diego"; 
+
+
+// FUNCIONES
+
+hey ();  // se llama la funcion
+
+//se inicializa
+
+function hey () {
+    console.log ("Hola " + miNombre);
+}
