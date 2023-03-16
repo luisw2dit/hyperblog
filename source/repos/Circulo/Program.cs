bb53519fb9c3c40d76401c43583c797a5fc5c660
Luis Alfredo Muñoz Zabaleta <luis.munoz@w2dit.com>
2023-03-16 18:24:11 -0500

    Ejercicios en clase de JS y C#

---
+++ source/repos/Circulo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+var radio = 0d;
+var resultado = 0d;
+
+const double PI = 3.14;
+
+Console.WriteLine("Bienvenidos calculemos circulos");
+
+//pi*radio*radio
+
+Console.WriteLine("Ingrese el valor del radio ");
+radio = Convert.ToDouble(Console.ReadLine ());
+
+resultado = PI * radio + radio;
+
+Console.WriteLine("E area de tu circulo es de:  " + resultado);
---
+++ source/repos/helloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Programa que calcula el area de un rectangulo
+
+var ladoA = 1.1;
+var ladoB = 1.1;
+var resultado 1.1;
+
+Console.WriteLine("Calcula el area de un rectangulo!!");
+
+Console.WriteLine("Ingrese el valor del lado A");
+ladoA = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+
+Console.WriteLine("El lado A es: " + ladoA +"El lado B es:  " + ladoB + "El resultado es " + resultado);
