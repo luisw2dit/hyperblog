0a564f91b715689101c63edbba87edc543a00397
Luis Alfredo Muñoz Zabaleta <luis.munoz@w2dit.com>
2023-04-10 17:57:37 -0500

    ejercicios de JavaScript

---
+++ Curso-basico-JS-Poo/main.js
@@ -0,0 +1,46 @@
+const natalia ={
+name: "Natalia",
+age: 20,
+cursosAprobados: [
+    "Curso Definitivo de HTML y CSS ",
+    "Curso Práctico de HTML y CSS",
+],
+
+// Crear metodo, crear un nuevo curso
+aprobarCurso(nuevoCursito)
+{this.cursosAprobados.push (nuevoCursito);   } 
+
+
+}
+
+
+// CREAR PROTOTIPO - Es crear un molde  = new
+
+function Student (name, age, cursosAprobados) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados  
+}
+
+Student.prototype.aprobarCurso = function (nuevoCursito)
+{this.cursosAprobados.push(nuevoCursito); }
+
+const juanita = new Student (
+    "Juanita Alejandra",
+    15,
+
+    [
+        "Curso de Introducción a la producción de Videojuegos",
+        "Curso de Creacion de Personajes",
+    ],
+
+);
+
+
+
+
+
+
+// Hacer que Natalia apruebe otro curso 
+
+natalia.cursosAprobados.push("Curso de Responsive Design");
